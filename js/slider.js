a4972128ef6cbb288ba06039d77f3d448bd8b0d1
Nodesus <77641921+Nodesus@users.noreply.github.com>
2021-04-08 23:11:14 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 const slider = $('.slider__list').bxSlider({
   pager: false,
   controls: false,
-  touchEnabled: ($(window).width() < 769),
+  touchEnabled: ($(window).width() < 769)
 });
 
 $(".slider__btn-left").click(e => {
